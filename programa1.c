62b1b2d0b8126a33669cc36e6acb9dcce5746064
Justjuliko <83980281+Justjuliko@users.noreply.github.com>
2022-05-13 09:44:23 -0500

    La chimba ps

--- programa1.c
+++ programa1.c
@@ -9,15 +9,24 @@ mqd_t mq1;
 
 void *send_file(void *param)
 {
-    mqd_t mq1 = mq_open("/mq1", O_WRONLY);
     char buffer[64];
+    char datos[20];
 
     while (1)
     {
         fgets(buffer, sizeof(buffer), stdin);
         if (buffer[strlen(buffer) - 1] == '\n')
             buffer[strlen(buffer) - 1] = 0;
-        mq_send(mq1, buffer, strlen(buffer) + 1, 0);
+        FILE *Text = fopen(buffer, "r");
+
+        for (int i = 0; i < 10; i++)
+        {
+            fgets((char *)(datos + i), 20, Text);
+            mq_send(mq1, (datos+i), strlen(datos) + 1, 0);
+        }           
+        fgets(buffer, sizeof(buffer), stdin);
+        if (buffer[strlen(buffer) - 1] == '\n')
+            buffer[strlen(buffer) - 1] = 0;
     }
 }
 
@@ -39,9 +48,10 @@ int main(int argc, char *argv[])
 
     mq = mq_open("/mq0", O_RDONLY | O_CREAT, 0644, &attr);
 
-    mq1 = mq_open("/mq1", O_RDONLY | O_CREAT, 0644, &attr1);
+    mq1 = mq_open("/mq1", O_WRONLY | O_CREAT, 0644, &attr1);
 
     char buff[32];
+    int order;
 
     pthread_t thread_ID;
     pthread_create(&thread_ID, NULL, &send_file, NULL);
@@ -49,22 +59,28 @@ int main(int argc, char *argv[])
     char datos[20][21];
 
     while (1)
-    {
+    {   
+        for (int i = 0; i < 10; i++)
+        {
+            mq_receive(mq, (datos[0]+i), 32, NULL);
+        } 
         mq_receive(mq, buff, 32, NULL);
-        FILE *Text = fopen(buff, "r");
-        for (int i = 0; i < 10; i++)
+        sscanf(buff, "%d", &order);
+        switch (order)
         {
-            fgets((char *)(datos + i), 20, Text);
-        }
-        for (int i = 0; i < 10; i++)
-        {
-            printf("%d:%s", i, (char *)(datos + i));
-        }
+        case 1:
+            for (int i = 0; i < 10; i++)
+            {
+                printf("%d:%s", i, (char *)(datos + i));
+            }
+            break;
 
-        //    for (int i = 10; i > -1; i--)
-        //   {
-        //        printf("%d:%s", i, (char *)(datos - i));
-        //    }
+        case 2:
+            for (int i = 9; i > -1; i--)
+            {
+                printf("%d:%s", i, (char *)(datos + i));
+            }
+            break;
+        }
     }
 }
-
--- programa2.c
+++ programa2.c
@@ -5,9 +5,8 @@
 #include <mqueue.h>
 #include <pthread.h>
 
-void* receive_file(void *param)
+void *receive_file(void *param)
 {
-
 
     char datos[20][21];
 
@@ -15,16 +14,29 @@ void* receive_file(void *param)
     {
         mqd_t mq1 = mq_open("/mq1", O_RDONLY);
         char buff[32];
+        int order;
 
+        for (int i = 0; i < 10; i++)
+        {
+            mq_receive(mq1, (datos[i]+i), 32, NULL);
+        } 
         mq_receive(mq1, buff, 32, NULL);
-        FILE *Text = fopen(buff,"r");
-        for(int i=0; i < 10; i++)
+        sscanf(buff, "%d", &order);
+        switch (order)
         {
-            fgets((char*)(datos+i),20,Text);
-        }
-        for(int i=0; i < 10; i++)
-        {
-            printf("%d:%s",i,(char*)(datos+i));
+        case 1:
+            for (int i = 0; i < 10; i++)
+            {
+                printf("%d:%s", i, (char *)(datos + i));
+            }
+            break;
+
+        case 2:
+            for (int i = 9; i > -1; i--)
+            {
+                printf("%d:%s", i, (char *)(datos + i));
+            }
+            break;
         }
     }
 }
@@ -32,15 +44,28 @@ void* receive_file(void *param)
 int main(int argc, char *argv[])
 {
     mqd_t mq = mq_open("/mq0", O_WRONLY);
-    char buffer[64];
 
     pthread_t threadID;
     pthread_create(&threadID, NULL, &receive_file, NULL);
 
+    char buffer[64];
+    char datos[20];
+
     while (1)
     {
-        fgets(buffer, sizeof(buffer),stdin);
-        if(buffer[strlen(buffer) - 1 ] == '\n') buffer[strlen(buffer) - 1] = 0;
-        mq_send(mq, buffer, strlen(buffer) + 1, 0);
+        fgets(buffer, sizeof(buffer), stdin);
+        if (buffer[strlen(buffer) - 1] == '\n')
+            buffer[strlen(buffer) - 1] = 0;
+        FILE *Text = fopen(buffer, "r");
+
+        for (int i = 0; i < 10; i++)
+        {
+            fgets((char *)(datos + i), 20, Text);
+            mq_send(mq, datos, strlen(datos) + 1, 0);
+        }           
+        fgets(buffer, sizeof(buffer), stdin);
+        if (buffer[strlen(buffer) - 1] == '\n')
+            buffer[strlen(buffer) - 1] = 0;
+        mq_send(mq, buffer, strlen(buffer), 0);
     }
 }
